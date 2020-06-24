2d646dd711fad5245acd1073985670fad3e8c26e
vaibhavshinde <vaibhav_shinde@codolyte.com>
2020-06-25 01:55:14 +0530

    reverse array using functional approch

--- Arrayreverse/reversearray.js
+++ Arrayreverse/reversearray.js
@@ -7,11 +7,11 @@
 
 
  /**
-  * Time Complexity : O(n) _ Average Case
+  * Time Complexity : O(n) - Average Case
   * Space Complexity : O(n) - auxillary space
   * @param {Array} arr input array
   */
- 
+ /*
  function reverse(arr){
      let reverse = [];
      if(! arr instanceof Array){
@@ -22,13 +22,14 @@
      }
      return reverse;
  }
-
+*/
 
 /**
  * Time Complexity : O(n/2) _ Average Case
   * Space Complexity : O(1) - auxillary space
  * @param {Array} arr 
  */
+/*
  function reverse(arr){
      let temp = 0;
      for(let i=0,j=arr.length-1; i<j;i++,j--){
@@ -43,3 +44,19 @@
  console.log(arr);
  reverse(arr);
  console.log(arr);
+
+
+*/
+
+
+
+
+ // more functional approch
+
+ const reverse = (arr) => {
+     return arr.map((element, index, callbackArr) => callbackArr[(callbackArr.length-1) - index]);
+ }
+
+ let arr = [1,2,3,5];
+ console.log(arr);
+ console.log(reverse(arr));
